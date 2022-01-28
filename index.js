38463cb56a0fe9812f428d91b075676a0040560b
darshan1059 <chandradarshanagrawal1059@gmail.com>
2022-01-28 23:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${MY_KEY}`;
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
 
 async function getWeatherByLocation(city) {
     const resp = await fetch(url(city), {
